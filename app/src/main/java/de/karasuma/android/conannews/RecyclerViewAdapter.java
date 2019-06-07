8c9802bd28c49a75eeec4fd1493d99ebb3ac3fe0
Karasuma1412 <bjoerninorek@gmail.com>
2019-06-07 16:16:55 +0200

    removed content from posts display

--- app/src/main/java/de/karasuma/android/conannews/RecyclerViewAdapter.java
+++ app/src/main/java/de/karasuma/android/conannews/RecyclerViewAdapter.java
@@ -33,7 +33,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     public void onBindViewHolder(@NonNull PostsViewHolder postsViewHolder, int i) {
         postsViewHolder.title.setText(posts.get(i).getTitle());
-        postsViewHolder.content.setText(posts.get(i).getContent());
         postsViewHolder.cover.setImageBitmap(posts.get(i).getBitmap());
     }
 
