065a9b3c6c0d0b83c54b1e5b6abcc7f194f98b5d
Karasuma1412 <bjoerninorek@gmail.com>
2019-06-30 09:58:16 +0200

    changed scroll view to match parent

--- app/src/main/java/de/karasuma/android/conannews/communication/html/HTMLParser.java
+++ app/src/main/java/de/karasuma/android/conannews/communication/html/HTMLParser.java
@@ -125,7 +125,8 @@ class HTMLParser {
     }
 
     public static View parseArticle(Element articleElement, final PostActivity postActivity) {
-        LinearLayout view = new LinearLayout(postActivity);
+        LinearLayout view = (LinearLayout) postActivity.getLayoutInflater().inflate(R.layout.article_layout, null, false);;
+        view.setBackgroundColor(Color.RED);
         view.setOrientation(LinearLayout.VERTICAL);
 
         //get article tag
@@ -180,7 +181,8 @@ class HTMLParser {
                 try {
                     URL imageURL = new URL(imageURLString);
                     Bitmap image = BitmapFactory.decodeStream(imageURL.openConnection().getInputStream());
-                    ImageView imageView = new ImageView(postActivity);
+                    ImageView imageView = (ImageView) postActivity.getLayoutInflater().inflate(R.layout.article_image, null, false);
+//                    ImageView imageView = new ImageView(postActivity);
                     imageView.setImageBitmap(image);
                     contentLayout.addView(imageView);
                 } catch (MalformedURLException exception) {
